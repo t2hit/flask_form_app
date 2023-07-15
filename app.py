57ac4c872bb516f48aad61de5960765ef4553100
t2hit <t.matsukusa@gmail.com>
2023-07-15 13:58:41 +0900

    refactoringf done

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=["Get", "Post"])
-def index():
-    if request.method == "GET":
-        return render_template("index.html")
-    else:
-        message = request.form["message"]
-        return render_template("index.html", message=message)
+@app.route("/", methods=["Get"])
+def index_get():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["Post"])
+def index_post():
+    message = request.form["message"]
+    return render_template("index.html", message=message)
 
 
 if __name__ == "__main__":
